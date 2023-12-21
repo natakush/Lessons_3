1ed074602a9e77e75b02d8b0908ddc91eb3edd1f
nata <natakush.job@gmail.com>
2023-12-21 22:56:52 +0200

    lecture7

---
+++ src/main/java/hw7/Main.java
@@ -0,0 +1,25 @@
+package hw7;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Users> list = new ArrayList<>();
+        list.add(new Users ("Nata", "Kyiv", "Ukraine", 55));
+        list.add(new Users ("Roman", "Lviv","Ukraine", 50));
+        list.add(new Users ("Mary", "NY","United States", 15));
+        list.add(new Users ("Petro", "Kyiv","Ukraine", 40));
+
+        List<Users> filteredlist = new ArrayList<>();
+        for (int i=0; i<list.size(); i++) {
+            if (list.get(i).getCountry().equals("Ukraine")) {
+                filteredlist.add(list.get(i));
+            }
+        }
+
+        Collections.sort(filteredlist);
+        filteredlist.forEach(System.out::println);
+    }
+}
