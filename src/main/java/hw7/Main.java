275be1557bff647e78166d2054e52f40ef3bc5e5
nata <natakush.job@gmail.com>
2024-01-16 19:31:34 +0200

    changes

--- src/main/java/hw7/Main.java
+++ src/main/java/hw7/Main.java
@@ -1,8 +1,6 @@
 package hw7;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,5 +19,53 @@ public class Main {
 
         Collections.sort(filteredlist);
         filteredlist.forEach(System.out::println);
+
+        Map<String, String> Meal1 = new HashMap<>();
+        Meal1.put("mealName","Pizza Verona");
+        Meal1.put("eatingType","fast");
+        Meal1.put("cookingTime","40");
+
+        Map<String, String> Meal2 = new HashMap<>();
+        Meal2.put("mealName","Sushi Dragon");
+        Meal2.put("eatingType","slow");
+        Meal2.put("cookingTime","115");
+
+        List<Map> Datalist = new ArrayList<>();
+        Datalist.add(Meal1);
+        Datalist.add(Meal2);
+
+        Map<String, List> Data = new HashMap<>();
+        Data.put("data",Datalist);
+
+        Map<String, String> PaginationData = new HashMap<>();
+        PaginationData.put("total","4308");
+        PaginationData.put("limit","2");
+        PaginationData.put("offset","30");
+
+        Map<String, Map> Pagination = new HashMap<>();
+        Pagination.put("pagination",PaginationData);
+
+        List<Map> Json = new ArrayList<>();
+        Json.add(Data);
+        Json.add(Pagination);
+
+        System.out.println();
+        System.out.println("{"+Json.get(0)+", "+Json.get(1)+"}");
+        System.out.println();
+
+        Datalist.add(new HashMap<>(Map.of("mealName", "PizzaSSSS")));
+        System.out.println("New object added to 'data' list");
+        System.out.println("{"+Json.get(0)+", "+Json.get(1)+"}");
+        System.out.println();
+
+        System.out.println("Searching for pizza in our json");
+        List<Map> SearchResult = new ArrayList<>();
+        for (int i=0; i<Datalist.size(); i++) {
+            if (Datalist.get(i).get("mealName").toString().contains("Pizza")) {
+                SearchResult.add(Datalist.get(i));
+            }
+        }
+        System.out.println(SearchResult);
+        System.out.println(PaginationData);
     }
 }
